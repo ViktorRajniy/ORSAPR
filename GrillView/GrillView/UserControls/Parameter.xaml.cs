04aa95183ef3e417cecd55b3f029f922e6119724
Виктор Ражный <rajniyviktor@yandex.ru>
2023-11-05 21:08:45 +0700

    (GrillView): Добавлено решение View

---
+++ GrillView/GrillView/UserControls/Parameter.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GrillView.UserControls
+{
+	/// <summary>
+	/// Логика взаимодействия для Parameter.xaml
+	/// </summary>
+	public partial class Parameter : UserControl
+	{
+		public Parameter()
+		{
+			InitializeComponent();
+		}
+
+		private string _parameterName;
+
+		private string _parameterValue;
+
+		private string _parameterBorder;
+
+		public string ParameterName
+		{
+			get { return _parameterName; }
+			set
+			{
+				_parameterName = value;
+				parameterName.Content = _parameterName;
+			}
+		}
+
+		public string ParameterValue
+		{
+			get { return _parameterValue; }
+			set
+			{
+				_parameterValue = value;
+				parameterTextBox.Text = _parameterValue;
+			}
+		}
+
+		public string ParameterBorder
+		{
+			get { return _parameterBorder; }
+			set
+			{
+				_parameterBorder = value;
+				parameterBorder.Content = _parameterBorder;
+			}
+		}
+	}
+}
