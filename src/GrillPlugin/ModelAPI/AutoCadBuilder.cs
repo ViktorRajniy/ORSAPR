02dad7a3b947c113a755a66751103af35bc6e390
Виктор Ражный <rajniyviktor@yandex.ru>
2023-12-04 07:09:02 +0700

    fix/(Builder): Добавлена заглушка

--- src/GrillPlugin/ModelAPI/AutoCadBuilder.cs
+++ src/GrillPlugin/ModelAPI/AutoCadBuilder.cs
@@ -2,10 +2,9 @@
 {
     using Autodesk.AutoCAD.ApplicationServices;
     using Autodesk.AutoCAD.DatabaseServices;
-    // using Autodesk.AutoCAD.Geometry;
     using CadApplication = Autodesk.AutoCAD.ApplicationServices.Core.Application;
     using GrillPlugin.Model;
-    // using Autodesk.AutoCAD.Geometry;
+    using Autodesk.AutoCAD.Geometry;
 
     /// <summary>
     /// Класс строитель мангала.
@@ -36,6 +35,28 @@
 
         public void BuildGrill()
         {
+            var document = Application.DocumentManager.MdiActiveDocument;
+            var database = document.Database;
+
+            using (var transaction = database.TransactionManager.StartTransaction())
+            {
+                var blockTable = transaction.GetObject(database.BlockTableId, OpenMode.ForRead)
+                    as BlockTable;
+
+                var blockTableRecords =
+                    transaction.TransactionManager
+                        .GetObject(
+                            blockTable[BlockTableRecord.ModelSpace],
+                            OpenMode.ForWrite) as BlockTableRecord;
+
+                var circle = new Circle();
+                circle.Radius = 1;
+                circle.Center = new Point3d(0, 0, 0);
+
+                blockTableRecords?.AppendEntity(circle);
+                transaction.AddNewlyCreatedDBObject(circle, true);
+                transaction.Commit();
+            }
         }
 
         private Solid3d BuildGrillBox()
