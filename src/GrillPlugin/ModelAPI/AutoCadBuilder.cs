b95371824181cd6339f76bcd39e90eed0d571841
Виктор Ражный <rajniyviktor@yandex.ru>
2023-12-04 07:04:26 +0700

    features(ModelAPI): Добавлена обёртка API и создан проект Builder

---
+++ src/GrillPlugin/ModelAPI/AutoCadBuilder.cs
@@ -0,0 +1,57 @@
+﻿namespace ModelAPI
+{
+    using Autodesk.AutoCAD.ApplicationServices;
+    using Autodesk.AutoCAD.DatabaseServices;
+    // using Autodesk.AutoCAD.Geometry;
+    using CadApplication = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+    using GrillPlugin.Model;
+    // using Autodesk.AutoCAD.Geometry;
+
+    /// <summary>
+    /// Класс строитель мангала.
+    /// </summary>
+    public class AutoCadBuilder
+    {
+        private readonly Parameters _parameters;
+
+        /// <summary>
+        /// Активный документ (чертеж) в AutoCAD.
+        /// </summary>
+        private Document _document;
+
+        /// <summary>
+        /// База данных документа.
+        /// </summary>
+        private Database _database;
+
+        /// <summary>
+        /// Конструктор класса с вложенными параметрами.
+        /// </summary>
+        /// <param name="parameters"> словарь с параметрами модели. </param>
+        public AutoCadBuilder(Parameters parameters)
+        {
+            _parameters = parameters;
+            InitialSetting();
+        }
+
+        public void BuildGrill()
+        {
+        }
+
+        private Solid3d BuildGrillBox()
+        {
+            return new Solid3d();
+        }
+
+        /// <summary>
+        /// Преднастройка.
+        /// </summary>
+        private void InitialSetting()
+        {
+            _document = CadApplication.DocumentManager.MdiActiveDocument;
+            _database = _document.Database;
+
+            _database.Insunits = UnitsValue.Millimeters;
+        }
+    }
+}
---
+++ src/GrillPlugin/ModelAPI/AutoCadWrapper.cs
@@ -0,0 +1,34 @@
+﻿namespace GrillPlugin.Model
+{
+    // using Autodesk.AutoCAD.ApplicationServices;
+    // using Autodesk.AutoCAD.DatabaseServices;
+    using System.Windows.Forms;
+    using Autodesk.AutoCAD.Runtime;
+    using ModelAPI;
+
+    /// <summary>
+    /// Обёртка для работы с API.
+    /// </summary>
+    public class AutoCadWrapper
+    {
+        /// <summary>
+        /// Команда вызывающая запуск плагина.
+        /// </summary>
+        [CommandMethod("Grill")]
+        public void StartPlaguin()
+        {
+            GrillPluginForm grillPluginForm = new GrillPluginForm();
+            DialogResult dialogResult = grillPluginForm.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                BuildGrill(grillPluginForm.parameters);
+            }
+        }
+
+        public void BuildGrill(Parameters parameters)
+        {
+            AutoCadBuilder builder = new AutoCadBuilder(parameters);
+            builder.BuildGrill();
+        }
+    }
+}
