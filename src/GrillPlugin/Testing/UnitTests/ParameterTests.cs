5a3d976f73a5e570de8c7d28db9db788a69d4b37
Виктор Ражный <rajniyviktor@yandex.ru>
2023-12-16 22:56:27 +0700

    features(tests): Добавлены тесты

---
+++ src/GrillPlugin/Testing/UnitTests/ParameterTests.cs
@@ -0,0 +1,160 @@
+﻿namespace UnitTests
+{
+    [TestFixture]
+    public class ParameterTests
+    {
+        [Test(Description = "Позитивный тест сеттера Value")]
+        public void ParameterValue_SetCorrectValue_ValueIsSetted()
+        {
+            // Setup:
+            double expected = 10;
+
+            Parameter actual = new Parameter(5, 0, 15);
+
+            // Testing:
+            actual.Value = expected;
+
+            // Assert:
+            Assert.AreEqual(expected, actual.Value);
+        }
+
+        [Test(Description = "Позитивный тест геттера Value")]
+        public void ParameterValue_GetCorrectValue_ValueIsGetted()
+        {
+            // Setup:
+            double expected = 5;
+
+            Parameter actual = new Parameter(5, 0, 15);
+
+            // Testing:
+
+            // Assert:
+            Assert.That(actual.Value, Is.EqualTo(expected));
+        }
+
+        [Test(Description = "Позитивный тест сеттера MinValue")]
+        public void ParameterMinValue_SetCorrectValue_ValueIsSetted()
+        {
+            // Setup:
+            double expected = 10;
+
+            Parameter actual = new Parameter(12, 0, 15);
+
+            // Testing:
+            actual.MinValue = expected;
+
+            // Assert:
+            Assert.AreEqual(expected, actual.MinValue);
+        }
+
+        [Test(Description = "Позитивный тест геттера MinValue")]
+        public void ParameterMinValue_GetCorrectValue_ValueIsGetted()
+        {
+            // Setup:
+            double expected = 0;
+
+            Parameter actual = new Parameter(12, 0, 15);
+
+            // Testing:
+
+            // Assert:
+            Assert.That(actual.MinValue, Is.EqualTo(expected));
+        }
+
+        [Test(Description = "Позитивный тест сеттера MaxValue")]
+        public void ParameterMaxValue_SetCorrectValue_ValueIsSetted()
+        {
+            // Setup:
+            double expected = 10;
+
+            Parameter actual = new Parameter(7, 0, 15);
+
+            // Testing:
+            actual.MaxValue = expected;
+
+            // Assert:
+            Assert.AreEqual(expected, actual.MaxValue);
+        }
+
+        [Test(Description = "Позитивный тест геттера MaxValue")]
+        public void ParameterMaxValue_GetCorrectValue_ValueIsGetted()
+        {
+            // Setup:
+            double expected = 15;
+
+            Parameter actual = new Parameter(12, 0, 15);
+
+            // Testing:
+
+            // Assert:
+            Assert.That(actual.MaxValue, Is.EqualTo(expected));
+        }
+
+        [Test(Description = "Позитивный тест конструктора Parameter")]
+        public void Parameter_ValueIsSetted()
+        {
+            // Setup:
+            double expectedValue = 10;
+            double expectedMinValue = 5;
+            double expectedMaxValue = 15;
+
+            Parameter actual = new Parameter(10, 5, 15);
+
+            // Testing:
+
+            // Assert:
+            Assert.Multiple(
+                () =>
+                {
+                    Assert.AreEqual(expectedValue, actual.Value);
+                    Assert.AreEqual(expectedMinValue, actual.MinValue);
+                    Assert.AreEqual(expectedMaxValue, actual.MaxValue);
+                });
+        }
+
+        [Test(Description = "Негативный тест метода MinMaxValidate")]
+        public void MinMaxValidate_GetIncorrectBigValue_TestNotPassed()
+        {
+            // Setup:
+
+            // Testing:
+
+            // Assert:
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    Parameter setup = new Parameter(10, 0, 5);
+                });
+        }
+
+        [Test(Description = "Негативный тест метода MinMaxValidate")]
+        public void MinMaxValidate_GetIncorrectLittleValue_TestNotPassed()
+        {
+            // Setup:
+
+            // Testing:
+
+            // Assert:
+            Assert.Throws<ArgumentException>(
+                () =>
+                {
+                    Parameter setup = new Parameter(2, 5, 10);
+                });
+        }
+
+        [Test(Description = "Позитивный тест метода MinMaxValidate")]
+        public void MinMaxValidate_GetCorrectValue_TestPassed()
+        {
+            // Setup:
+
+            // Testing:
+
+            // Assert:
+            Assert.DoesNotThrow(
+                () =>
+                {
+                    Parameter setup = new Parameter(5, 0, 10);
+                });
+        }
+    }
+}
---
+++ src/GrillPlugin/Testing/UnitTests/ParametersTests.cs
@@ -0,0 +1,83 @@
+﻿namespace UnitTests
+{
+    public class ParametersTests
+    {
+        [Test(Description = "Позитивный тест метода InitHoleGrooveCount")]
+        public void InitHoleGrooveCount_ValueCorrect()
+        {
+            // Setup:
+            Parameters setup = new Parameters();
+
+            int expectedHoleCount = 24;
+            int expectedGrooveCount = 49;
+
+            // Testing:
+            setup.InitHoleGrooveCount();
+
+            // Assert:
+            Assert.Multiple(
+                () =>
+                {
+                    Assert.That(setup.GetHoleCount(), Is.EqualTo(expectedHoleCount));
+                    Assert.That(setup.GetGrooveCount(), Is.EqualTo(expectedGrooveCount));
+                });
+        }
+
+        [Test(Description = "Позитивный тест метода UpdateBorders")]
+        public void UpdateBorders_ValueCorrect()
+        {
+            // Setup:
+            Parameters setup = new Parameters();
+
+            setup.SetValue(ParameterType.BoxLength, 1500);
+            setup.SetValue(ParameterType.BoxWidth, 400);
+            setup.SetValue(ParameterType.BoxHeight, 450);
+            setup.SetValue(ParameterType.BoxWallThickness, 6);
+            setup.SetValue(ParameterType.LegHeight, 750);
+            setup.SetValue(ParameterType.LegDiameter, 16);
+            setup.SetValue(ParameterType.GrooveDiameter, 12);
+            setup.SetValue(ParameterType.GrooveDistance, 22);
+            setup.SetValue(ParameterType.HoleDiameter, 50);
+            setup.SetValue(ParameterType.HoleHeight, 67);
+            setup.SetValue(ParameterType.HoleDistance, 50);
+
+            double expectedMinLegDiameter = 12;
+            double expectedMaxLegDiameter = 200;
+
+            double expectedMinGrooveDistance = 12;
+            double expectedMaxGrooveDistance = 1476;
+
+            double expectedMinHoleDiameter = 10;
+            double expectedMaxHoleDiameter = 225;
+
+            double expectedMinHoleHeight = 31;
+            double expectedMaxHoleHeight = 206;
+
+            double expectedMinHoleDistance = 50;
+            double expectedMaxHoleDistance = 1438;
+
+            // Testing:
+            setup.UpdateBorders();
+
+            // Assert:
+            Assert.Multiple(
+                () =>
+                {
+                    Assert.That(setup.GetParameter(ParameterType.LegDiameter).MinValue, Is.EqualTo(expectedMinLegDiameter));
+                    Assert.That(setup.GetParameter(ParameterType.LegDiameter).MaxValue, Is.EqualTo(expectedMaxLegDiameter));
+
+                    Assert.That(setup.GetParameter(ParameterType.GrooveDistance).MinValue, Is.EqualTo(expectedMinGrooveDistance));
+                    Assert.That(setup.GetParameter(ParameterType.GrooveDistance).MaxValue, Is.EqualTo(expectedMaxGrooveDistance));
+
+                    Assert.That(setup.GetParameter(ParameterType.HoleDiameter).MinValue, Is.EqualTo(expectedMinHoleDiameter));
+                    Assert.That(setup.GetParameter(ParameterType.HoleDiameter).MaxValue, Is.EqualTo(expectedMaxHoleDiameter));
+
+                    Assert.That(setup.GetParameter(ParameterType.HoleHeight).MinValue, Is.EqualTo(expectedMinHoleHeight));
+                    Assert.That(setup.GetParameter(ParameterType.HoleHeight).MaxValue, Is.EqualTo(expectedMaxHoleHeight));
+
+                    Assert.That(setup.GetParameter(ParameterType.HoleDistance).MinValue, Is.EqualTo(expectedMinHoleDistance));
+                    Assert.That(setup.GetParameter(ParameterType.HoleDistance).MaxValue, Is.EqualTo(expectedMaxHoleDistance));
+                });
+        }
+    }
+}
